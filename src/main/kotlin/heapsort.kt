7771b0666f4047cca9483b85da54eb27fd5c2442
Aleksandr Lisianoi <alisianoi@protonmail.com>
2019-11-10 13:41:24 +0100

    Improve sink/swim/heapify: T is either comparable, or need Comparator<T>

--- src/main/kotlin/heapsort.kt
+++ src/main/kotlin/heapsort.kt
@@ -1,5 +1,31 @@
+fun <T> swim(xs: MutableList<T>, index: Int): MutableList<T> where T : Comparable<T> {
+    return swim(xs, index, Comparator { x, y -> x.compareTo(y) })
+}
+
+fun <T> swim(xs: MutableList<T>, index: Int, comparator: Comparator<T>): MutableList<T> {
+    var i = index
+
+    while (i != 0) {
+        val parent = (i - 1) / 2
+
+        if (comparator.compare(xs[parent], xs[index]) <= 0) {
+            break
+        }
+
+        xs[parent] = xs[index].also { xs[index] = xs[parent] }
+
+        i = parent
+    }
+
+    return xs
+}
+
 fun <T> sink(xs: MutableList<T>, index: Int): MutableList<T> where T : Comparable<T> {
-    if (index < 0 || index >= xs.size / 2) {
+    return sink(xs, index, Comparator { x, y -> x.compareTo(y) })
+}
+
+fun <T> sink(xs: MutableList<T>, index: Int, comparator: Comparator<T>): MutableList<T> {
+    if (index < 0 || xs.size <= 2 * index + 1) {
         return xs
     }
 
@@ -11,25 +37,29 @@ fun <T> sink(xs: MutableList<T>, index: Int): MutableList<T> where T : Comparabl
 
         val child = when {
             rightChild == xs.size -> leftChild
-            xs[leftChild] < xs[rightChild] -> leftChild
+            comparator.compare(xs[leftChild], xs[rightChild]) < 0 -> leftChild
             else -> rightChild
         }
 
-        if (xs[i] <= xs[child]) {
+        if (comparator.compare(xs[i], xs[child]) <= 0) {
             break
         }
 
         xs[i] = xs[child].also { xs[child] = xs[i] }
 
         i = child
-    } while (i < xs.size / 2)
+    } while (xs.size > 2 * i + 1)
 
     return xs
 }
 
 fun <T> heapify(xs: MutableList<T>): MutableList<T> where T : Comparable<T> {
+    return heapify(xs, Comparator { x, y -> x.compareTo(y) })
+}
+
+fun <T> heapify(xs: MutableList<T>, comparator: Comparator<T>): MutableList<T> {
     for (i in xs.size / 2 downTo 0) {
-        sink(xs, i)
+        sink(xs, i, comparator)
     }
 
     return xs
