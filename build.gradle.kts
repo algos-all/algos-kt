676bd3df410d482d38487e279197816f38aa7d44
Aleksandr Lisianoi <alisianoi@protonmail.com>
2020-12-23 21:32:46 +0100

    Update build.gradle.kts (remove most things, update JUnit 5)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,19 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     java
-    kotlin("jvm") version "1.3.41"
+    kotlin("jvm") version "1.4.20"
 }
 
-group = "com.github.alisianoi"
-version = "1.0-SNAPSHOT"
-
 repositories {
-    mavenCentral()
+    jcenter()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.withType<Test> {
+tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
