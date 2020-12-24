0bd1af733a6e7e9cda9bb249aa88bc2d8bac92d5
Aleksandr Lisianoi <alisianoi@protonmail.com>
2020-12-24 06:39:26 +0100

    Add 400+ tests for Heap

--- src/test/kotlin/HeapTest.kt
+++ src/test/kotlin/HeapTest.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 
@@ -38,5 +39,126 @@ class HeapTest {
 
     @Test
     fun `heap poll with two elements, reversed`() {
+        val heap = Heap(mutableListOf(42, 43)) { x, y ->
+            if (x > y) {
+                -1
+            } else {
+                +1
+            }
+        }
+
+        assert(heap.poll() == 43)
+        assert(heap.poll() == 42)
+        assert(heap.isEmpty())
+    }
+
+    @Test
+    fun `heap poll with five elements`() {
+        val heap = Heap(mutableListOf(42, 52, 48, 44, 50))
+
+        assert(heap.poll() == 42)
+        assert(heap.poll() == 44)
+        assert(heap.poll() == 48)
+        assert(heap.poll() == 50)
+        assert(heap.poll() == 52)
+        assert(heap.isEmpty())
+    }
+
+    @Test
+    fun `heap poll with five elements, reversed`() {
+        val heap = Heap(mutableListOf(42, 52, 48, 44, 50), reverseOrder())
+
+        assert(heap.poll() == 52)
+        assert(heap.poll() == 50)
+        assert(heap.poll() == 48)
+        assert(heap.poll() == 44)
+        assert(heap.poll() == 42)
+        assert(heap.isEmpty())
+    }
+
+    @RepeatedTest(value = 100)
+    fun `heap add random fuzz`() {
+        val heap = Heap<Int>()
+        val list = mutableListOf<Int>()
+
+        for (i in 0 until 10) {
+            val element = (-1024..+1024).random()
+
+            heap.add(element)
+            list.add(element)
+
+            list.sort()
+
+            val hTop = heap.peek()
+            val lTop = list[0]
+
+            assert(hTop == lTop) { "$hTop != $lTop" }
+        }
+    }
+
+    @RepeatedTest(value = 100)
+    fun `heap add random fuzz, reversed`() {
+        val heap = Heap<Int>(reverseOrder())
+        val list = mutableListOf<Int>()
+
+        for (i in 0 until 10) {
+            val element = (-1024..+1024).random()
+
+            heap.add(element)
+            list.add(element)
+
+            list.sortWith(reverseOrder())
+
+            val hTop = heap.peek()
+            val lTop = list[0]
+
+            assert(hTop == lTop) { "$hTop != $lTop" }
+        }
+    }
+
+    @RepeatedTest(value = 100)
+    fun `heap poll random fuzz`() {
+        val n = 10
+        val list = mutableListOf<Int>()
+
+        for (i in 0 until n) {
+            list.add((-1024..1024).random())
+        }
+
+        val heap = Heap(list)
+
+        for (i in 0 until n) {
+            list.sort()
+
+            val hTop = heap.poll()
+            val lTop = list[0]
+
+            assert(hTop == lTop) { "$hTop != $lTop" }
+
+            list.removeAt(0)
+        }
+    }
+
+    @RepeatedTest(value = 100)
+    fun `heap poll random fuzz, reversed`() {
+        val n = 10
+        val list = mutableListOf<Int>()
+
+        for (i in 0 until n) {
+            list.add((-1024..1024).random())
+        }
+
+        val heap = Heap(list, reverseOrder())
+
+        for (i in 0 until n) {
+            list.sortWith(reverseOrder())
+
+            val hTop = heap.poll()
+            val lTop = list[0]
+
+            assert(hTop == lTop) { "$hTop != $lTop" }
+
+            list.removeAt(0)
+        }
     }
 }
