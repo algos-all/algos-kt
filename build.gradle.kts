0a9bec1a348859247cb9cae805dbe19df8ce4b15
Aleksandr Lisianoi <alisianoi@protonmail.com>
2021-01-03 04:11:58 +0100

    Set target language level to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     java
     kotlin("jvm") version "1.4.20"
@@ -9,6 +11,10 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
 }
 
 tasks.withType<Test>().configureEach {
