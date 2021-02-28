cad18c62f47377701cabb7f9434e8868ba921916
Aleksandr Lisianoi <alisianoi@protonmail.com>
2021-03-01 00:34:17 +0100

    Add ToposortDFS and tests

---
+++ src/main/kotlin/graph/ToposortDFS.kt
@@ -0,0 +1,17 @@
+package graph
+
+/**
+ * Perform a topological sort of a directed graph using the Tarjan's algorithm
+ *
+ * A topological sort is possible if and only if the graph has no cycles
+ *
+ * @return a list of the vertices such that for every directed edge from vertex
+ *         u to vertex v in the graph, u comes before v in the ordering.
+ * @see: https://en.wikipedia.org/wiki/Topological_sorting
+ * @see: https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
+ */
+fun toposortDFS(graph: Graph): List<Int> {
+    val transposed = transpose(graph)
+    val initial = graph.keys.filter { transposed[it]?.isEmpty() == true }
+    return dfs(graph, initial.toHashSet()).reversed()
+}
