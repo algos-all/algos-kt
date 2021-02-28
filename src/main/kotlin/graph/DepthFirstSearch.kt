ff52ee606248a766e47f6fb600a3901120317fcf
Aleksandr Lisianoi <alisianoi@protonmail.com>
2021-02-28 16:42:00 +0100

    Add depth first search & tests for it

---
+++ src/main/kotlin/graph/DepthFirstSearch.kt
@@ -0,0 +1,62 @@
+package graph
+
+/**
+ * Given a graph, run depth first search starting at a random node
+ *
+ * @return the order in which the nodes were visited
+ */
+fun dfs(graph: Graph): List<Int> {
+    return if (graph.isEmpty()) listOf() else dfs(graph, graph.keys.random())
+}
+
+/**
+ * Given a graph and a starting node, run depth first search
+ *
+ * @return the order in which the nodes were visited
+ */
+fun dfs(graph: Graph, node: Int): List<Int> {
+    return dfs(graph, hashSetOf(node))
+}
+
+/**
+ * Given a graph and a set of starting nodes, run depth first search
+ *
+ * @return the order in which the nodes were visited
+ */
+fun dfs(graph: Graph, nodes: HashSet<Int>): List<Int> {
+    return dfs(graph, nodes, hashSetOf())
+}
+
+/**
+ * Run depth first search on a graph given a starting node and a skip set
+ *
+ * @param node the node to start from
+ * @param skips the nodes *not* to visit
+ * @return the order in which the nodes were visited
+ */
+fun dfs(graph: Graph, node: Int, skips: HashSet<Int>): List<Int> {
+    return dfs(graph, hashSetOf(node), skips)
+}
+
+/**
+ * Run depth first search on a graph gives a starting set and a skip set
+ *
+ * @param nodes the nodes to start from
+ * @param skips the nodes *not* to visit
+ * @return the order in which the nodes were visited
+ */
+fun dfs(graph: Graph, nodes: HashSet<Int>, skips: HashSet<Int>): List<Int> {
+    val order = mutableListOf<Int>()
+
+    for (node in nodes) {
+        skips.add(node)
+
+        graph[node]?.filter { !skips.contains(it) }?.forEach{
+            order.addAll(dfs(graph, it, skips))
+        }
+
+        order.add(node)
+    }
+
+    return order
+}
---
+++ src/test/kotlin/graph/TestDepthFirstSearch.kt
@@ -0,0 +1,66 @@
+package graph
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import strikt.api.expectThat
+import strikt.assertions.containsExactly
+import strikt.assertions.isA
+import strikt.assertions.isEmpty
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TestDepthFirstSearch {
+    @Test
+    fun `empty graph`() {
+        val graph = Graph()
+        expectThat(dfs(graph))
+            .isA<List<Int>>()
+            .isEmpty()
+    }
+
+    @Test
+    fun `one node`() {
+        val graph = Graph()
+        graph[42] = mutableListOf()
+        expectThat(dfs(graph))
+            .isA<List<Int>>()
+            .containsExactly(42)
+    }
+
+    @Test
+    fun `one edge`() {
+        val graph = Graph()
+        graph[42] = mutableListOf(43)
+        graph[43] = mutableListOf()
+        expectThat(dfs(graph, 42))
+            .isA<List<Int>>()
+            .containsExactly(43, 42)
+    }
+
+    @Test
+    fun `simple chain`() {
+        val graph = Graph()
+        graph[42] = mutableListOf(43)
+        graph[43] = mutableListOf(44)
+        graph[44] = mutableListOf()
+
+        expectThat(dfs(graph, 42))
+            .isA<List<Int>>()
+            .containsExactly(44, 43, 42)
+    }
+
+    @Test
+    fun `simple fork`() {
+        val graph = Graph()
+        graph[1] = mutableListOf(3)
+        graph[2] = mutableListOf(3)
+        graph[3] = mutableListOf(4)
+        graph[4] = mutableListOf()
+
+        expectThat(dfs(graph, 1))
+            .isA<List<Int>>()
+            .containsExactly(4, 3, 1)
+        expectThat(dfs(graph, 2))
+            .isA<List<Int>>()
+            .containsExactly(4, 3, 2)
+    }
+}
