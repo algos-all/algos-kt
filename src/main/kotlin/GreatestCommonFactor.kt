ae4c084806ced0dfcfa609d17de9933d097e8ab3
Aleksandr Lisianoi <alisianoi@protonmail.com>
2022-01-02 13:31:41 +0100

    Add extended greatest common factor

--- src/main/kotlin/GreatestCommonFactor.kt
+++ src/main/kotlin/GreatestCommonFactor.kt
@@ -14,3 +14,26 @@ fun gcf(a: Int, b: Int): Int {
 
     return x
 }
+
+/**
+ * Compute the extended greatest common factor of a pair of integers a and b
+ *
+ * @param a the first integer in the pair
+ * @param b the second integer in the pair
+ * @return (gcd, x, y) such that: a * x + b * y == gcd (Bezout's identity)
+ */
+fun xgcf(a: Int, b: Int): Array<Int> {
+    var (x, y) = listOf(a, b)
+    var (s1, s2) = listOf(1, 0)
+    var (t1, t2) = listOf(0, 1)
+
+    while (y != 0) {
+        val (q, r) = listOf(x / y, x % y)
+        x = y.also { y = r }
+
+        s2 = s1 - q * s2.also { s1 = s2 }
+        t2 = t1 - q * t2.also { t1 = t2 }
+    }
+
+    return arrayOf(x, s1, t1)
+}
--- src/test/kotlin/GreatestCommonFactorTest.kt
+++ src/test/kotlin/GreatestCommonFactorTest.kt
@@ -34,4 +34,26 @@ class GreatestCommonFactorTest {
         assert(gcf(78, 18) == 6)
         assert(gcf(18, 78) == 6)
     }
+
+    @Test
+    fun `extended greatest common factor of 1 and 2 is (1, 1, 0)`() {
+        assert(xgcf(1, 2).contentEquals(arrayOf(1, 1, 0)))
+    }
+
+    @Test
+    fun `extended greatest common factor of 2 and 1 is (1, 0, 1)`() {
+        assert(xgcf(2, 1).contentEquals(arrayOf(1, 0, 1)))
+    }
+
+    @Test
+    fun `extended greatest common factor of 12 and 16 is (4, -1, 1)`() {
+        assert(xgcf(12, 16).contentEquals(arrayOf(4, -1, 1)))
+        assert(xgcf(16, 12).contentEquals(arrayOf(4, 1, -1)))
+    }
+
+    @Test
+    fun `extended greatest common factor of 78 and 18 is (6, 1, -4)`() {
+        assert(xgcf(78, 18).contentEquals(arrayOf(6, 1, -4)))
+        assert(xgcf(18, 78).contentEquals(arrayOf(6, -4, 1)))
+    }
 }
