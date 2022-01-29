69490c10d27e02e6b2468a6b9893b7702bafef87
Aleksandr Lisianoi <alisianoi@protonmail.com>
2022-01-29 06:57:09 +0100

    Use JUnit BOM, print out a succinct report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+val versionJUnit = "5.8.2"
+val versionStrikt = "0.33.0"
 
 plugins {
     java
@@ -10,8 +14,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("io.strikt:strikt-core:0.33.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("io.strikt:strikt-core:$versionStrikt")
+    testImplementation(platform("org.junit:junit-bom:$versionJUnit"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.withType<KotlinCompile> {
@@ -21,6 +26,47 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
     testLogging {
-        events("passed", "skipped", "failed")
+        // Show the entire assertion, not just that it failed:
+        exceptionFormat = TestExceptionFormat.FULL
+        // Show only the problematic tests, skip the "passed" ones
+        events("skipped", "failed")
     }
+
+    addTestListener(object: TestListener {
+        override fun beforeSuite(suite: TestDescriptor?) {
+        }
+
+        override fun afterSuite(suite: TestDescriptor?, result: TestResult?) {
+            logger.trace("Enter afterSuite(suite, result)")
+
+            if (suite == null) {
+                logger.error("TestDescriptor is null, this is a bug!")
+                return
+            }
+            if (result == null) {
+                logger.error("TestResult is null, this is a bug!")
+                return
+            }
+
+            // This happens once at the end, when there is no parent test suite
+            if (suite.parent == null) {
+                val message = "  Total: ${result.testCount}"
+                val success = "Success: ${result.successfulTestCount}"
+                val skipped = "Skipped: ${result.skippedTestCount}"
+                val failure = "Failure: ${result.failedTestCount}"
+                val verdict = "Verdict: ${result.resultType}"
+
+                logger.warn("$message\n$success\n$skipped\n$failure\n$verdict")
+                return
+            }
+
+            logger.trace("Leave afterSuite(suite, result) (ok)")
+        }
+
+        override fun beforeTest(suite: TestDescriptor?) {
+        }
+
+        override fun afterTest(suite: TestDescriptor?, result: TestResult?) {
+        }
+    })
 }
