1cf400c2e52ac12b0d39c65e3e380ceeca1d516a
Aleksandr Lisianoi <alisianoi@protonmail.com>
2024-02-04 16:07:20 +0100

    Add Map<Int,Int>.getOrPut

--- src/test/kotlin/core/TestMap.kt
+++ src/test/kotlin/core/TestMap.kt
@@ -11,6 +11,7 @@ class TestMap {
         val result1 = map.getOrElse(13) { 44 }
         assert(result0 == 42) { "$result0 and 42" }
         assert(result1 == 44) { "$result1 and 44" }
+        assert(map[13] == null) { "map[13] still has old value" }
     }
 
     @Test
@@ -21,15 +22,29 @@ class TestMap {
         val result1 = map.getOrDefault(13, 44)
         assert(result0 == 42) { "$result0 and 42" }
         assert(result1 == 44) { "$result1 and 44" }
+        assert(map[13] == null) { " map[13] still has old value" }
+    }
+
+    @Test
+    fun testGetOrPut() {
+        val map = HashMap<Int, Int>()
+        map[42] = 42
+        val result0 = map.getOrPut(42) { 44 }
+        val result1 = map.getOrPut(13) { 44 }
+        assert(result0 == 42) { "$result0 is not null" }
+        assert(result1 == 44) { "$result1 is not null" }
+        assert(map[13] == 44) { "map[13] has the computed value" }
     }
 
     @Test
     fun testComputeIfAbsent() {
         val map = mutableMapOf<Int, Int>()
-        val result0 = map.putIfAbsent(42, 42)
-        val result1 = map.computeIfAbsent(44) { key -> key }
-        assert(result0 == null) { "$result0 is not null" }
+        map[42] = 42
+        val result0 = map.computeIfAbsent(42) { _ -> 44 }
+        val result1 = map.computeIfAbsent(13) { _ -> 44 }
+        assert(result0 == 42) { "$result0 is not null" }
         assert(result1 == 44) { "$result1 is not null" }
+        assert(map[13] == 44) { "map[13] has the computed value" }
     }
 
     @Test
