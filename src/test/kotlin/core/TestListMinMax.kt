99004ac8721b8b13fcff1d01195bbf2efa4649f7
Aleksandr Lisianoi <alisianoi@protonmail.com>
2024-02-17 12:18:06 +0100

    Add question-like structure

--- src/test/kotlin/core/TestListMinMax.kt
+++ src/test/kotlin/core/TestListMinMax.kt
@@ -7,8 +7,10 @@ import java.util.NoSuchElementException
 class TestListMinMax {
     @Test
     fun testMin() {
+        // Given a list:
         val xs = mutableListOf(42, 43, 44)
 
+        // What will be returned if you call xs.min()?
         val result = xs.min()
 
         assert(result == 42)
@@ -16,15 +18,19 @@ class TestListMinMax {
 
     @Test
     fun testMinThrow() {
+        // Given an empty list:
         val xs = emptyList<Int>()
 
+        // What will be returned if you call xs.min()?
         assertThrows<NoSuchElementException> { xs.min() }
     }
 
     @Test
     fun testMinOrNull() {
+        // Given a list whose content is unknown:
         val xs = emptyList<Int>()
 
+        // How to search for the min element without raising an exception?
         val result = xs.minOrNull()
 
         assert(result == null)
@@ -32,8 +38,10 @@ class TestListMinMax {
 
     @Test
     fun testMax() {
+        // Given a list:
         val xs = mutableListOf(42, 43, 44)
 
+        // What will be returned if you call xs.max()?
         val result = xs.max()
 
         assert(result == 44)
@@ -41,15 +49,19 @@ class TestListMinMax {
 
     @Test
     fun testMaxThrow() {
+        // Given an empty list:
         val xs = emptyList<Int>()
 
+        // What will be returned if you call xs.max()?
         assertThrows<NoSuchElementException> { xs.max() }
     }
 
     @Test
     fun testMaxOrNull() {
+        // Given a list whose content is unknown:
         val xs = emptyList<Int>()
 
+        // How to search for the max element without raising an exception?
         val result = xs.maxOrNull()
 
         assert(result == null)
