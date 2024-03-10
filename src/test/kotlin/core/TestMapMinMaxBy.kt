8db13e3c33f70b8624a4ec907ba55de85a22e0e2
Aleksandr Lisianoi <alisianoi@protonmail.com>
2024-03-10 16:39:22 +0100

    Improve TestMapMinMaxBy examples

--- src/test/kotlin/core/TestMapMinMaxBy.kt
+++ src/test/kotlin/core/TestMapMinMaxBy.kt
@@ -7,66 +7,119 @@ class TestMapMinMaxBy {
     @Test
     fun testMaxBy() {
         // Given a non-empty map:
-        val map = HashMap<Int, Int>()
-        map[42] = 42
-        map[43] = 43
-        map[44] = 44
+        data class Product(val name: String, val price: Int)
 
-        // How to find the entry yielding the largest value of the given function?
-        // Assume the function to be the value of the map entry:
-        val entry = map.maxBy { it.value }
+        val map = HashMap<Int, Product>()
+        map[42] = Product("apple", 42)
+        map[43] = Product("banana", 43)
+        map[44] = Product("mango", 44)
+
+        // Q: How to find the entry with the largest price?
+        val entry = map.maxBy { entry -> entry.value.price }
 
         assert(entry.key == 44)
-        assert(entry.value == 44)
+        assert(entry.value == Product("mango", 44))
+    }
+
+    @Test
+    fun testMaxByEmptyCase() {
+        // Given a non-empty map:
+        data class Product(val name: String, val price: Int)
+
+        val map = HashMap<Int, Product>()
+
+        // Q: How to handle an empty map when searching for most expensive product?
+
+        // Approach 1: allow null to be returned:
+        val entry1 = map.maxByOrNull { it.value.price }
+        assert(entry1 == null)
+
+        // Approach 2: catch the empty-case exception:
+        val entry2 = try {
+            map.maxBy { it.value.price }
+        } catch (e: NoSuchElementException) {
+            assert(true)
+        } catch (e: Throwable) {
+            assert(false)
+        }
+
+        assert(entry2 == Unit)
     }
 
     @Test
     fun testMaxByOrNull() {
         // Given a possibly empty map:
-        val map = HashMap<Int, Int>()
-        map[42] = 42
-        map[43] = 43
-        map[44] = 44
+        data class Product(val name: String, val price: Int)
 
-        // How to find the entry yielding the largest value of the given function?
-        // Assume the function to be the value of the map entry:
-        val entry = map.maxByOrNull { it.value }
+        val map = HashMap<Int, Product>()
+        map[42] = Product("apple", 42)
+        map[43] = Product("banana", 43)
+        map[44] = Product("mango", 44)
+
+        // Q: How to find the entry with the largest price? Allow null to be returned.
+        val entry = map.maxByOrNull { it.value.price }
 
         assert(entry != null)
         assert(entry?.key == 44)
-        assert(entry?.value == 44)
+        assert(entry?.value?.price == 44)
     }
 
     @Test
     fun testMinBy() {
         // Given a non-empty map:
-        val map = HashMap<Int, Int>()
-        map[42] = 42
-        map[43] = 43
-        map[44] = 44
+        data class Product(val name: String, val price: Int)
 
-        // How to find the entry yielding the smallest value of the given function?
-        // Assume the function to be the value of the map entry:
-        val entry = map.minBy { it.value }
+        val map = HashMap<Int, Product>()
+        map[42] = Product("apple", 42)
+        map[43] = Product("banana", 43)
+        map[44] = Product("mango", 44)
+
+        // Q: How to find the entry with the smallest price?
+        val entry = map.minBy { entry -> entry.value.price }
 
         assert(entry.key == 42)
-        assert(entry.value == 42)
+        assert(entry.value == Product("apple", 42))
+    }
+
+    @Test
+    fun testMinByEmptyCase() {
+        // Given a non-empty map:
+        data class Product(val name: String, val price: Int)
+
+        val map = HashMap<Int, Product>()
+
+        // Q: How to handle an empty map when searching for least expensive product?
+
+        // Approach 1: allow null to be returned:
+        val entry1 = map.minByOrNull { it.value.price }
+        assert(entry1 == null)
+
+        // Approach 2: catch the empty-case exception:
+        val entry2 = try {
+            map.minBy { it.value.price }
+        } catch (e: NoSuchElementException) {
+            assert(true)
+        } catch (e: Throwable) {
+            assert(false)
+        }
+
+        assert(entry2 == Unit)
     }
 
     @Test
     fun testMinByOrNull() {
         // Given a possibly empty map:
-        val map = HashMap<Int, Int>()
-        map[42] = 42
-        map[43] = 43
-        map[44] = 44
+        data class Product(val name: String, val price: Int)
 
-        // How to find the entry yielding the smallest value of the given function?
-        // Assume the function to be the value of the map entry:
-        val entry = map.minByOrNull { it.value }
+        val map = HashMap<Int, Product>()
+        map[42] = Product("apple", 42)
+        map[43] = Product("banana", 43)
+        map[44] = Product("mango", 44)
 
-        assert(entry != null)
+        // Q: How to find the entry with the smallest price? Allow null to be returned.
+        val entry = map.minByOrNull { it.value.price }
+
         assert(entry?.key == 42)
-        assert(entry?.value == 42)
+        assert(entry?.value == Product("apple", 42))
     }
 }
