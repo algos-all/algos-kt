3ed175ef14b52e879f0cc9632a3ae3c72addafe1
Aleksandr Lisianoi <alisianoi@protonmail.com>
2024-04-14 00:05:49 +0200

    Use standard mechanisms to parameterize FastPower tests

--- src/test/kotlin/math/FastPowerTest.kt
+++ src/test/kotlin/math/FastPowerTest.kt
@@ -2,63 +2,80 @@ package math
 
 import fpow0
 import fpow1
-import org.junit.jupiter.api.RepeatedTest
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+typealias FastPowerType = (x: Int, n: Int) -> Int
+
 class FastPowerTest {
-    private val functions = listOf<(x: Int, n: Int) -> Int>(
-        { x, n -> fpow0(x, n) },
-        { x, n -> fpow1(x, n) },
-    )
 
-    @RepeatedTest(value = 100)
-    fun `anything to the power of 0 is 1`() {
-        val x = (-1024..+1024).random()
-        functions.forEach { fastPower ->
-            assert(fastPower(x, 0) == 1)
+    companion object {
+        data class FastPowerWithIndex(val fastPower: FastPowerType, val index: Int)
+
+        @JvmStatic
+        fun fastPower(): List<FastPowerType> {
+            return listOf<FastPowerType>(::fpow0, ::fpow1)
         }
-    }
 
-    @RepeatedTest(value = 100)
-    fun `anything to the power of 1 is itself`() {
-        val x = (-1024..+1024).random()
-        functions.forEach { fastPower ->
-            assert(fastPower(x, 1) == x)
-        }
-    }
-
-    @RepeatedTest(value = 500)
-    fun `fast power works`() {
-        val x = (0..+42).random()
-        val n = (0..5).random()
-        functions.forEach { fastPower ->
-            assert(fastPower(x, n) == x.toDouble().pow(n).roundToInt()) {
-                "${fastPower(x, n)} is not ${x.toDouble().pow(n).roundToInt()}"
+        @JvmStatic
+        fun fastPowerWithIndex(): List<FastPowerWithIndex> {
+            return fastPower().flatMap { function ->
+                (0 until 500).map { index -> FastPowerWithIndex(function, index) }
             }
         }
     }
 
-    @RepeatedTest(value = 256)
-    fun `fast power (large power)`() {
+    @ParameterizedTest
+    @MethodSource("fastPowerWithIndex")
+    fun `anything to the power of 0 is 1`(fastPowerWithIndex: FastPowerWithIndex) {
+        val fastPower = fastPowerWithIndex.fastPower
+
+        val x = (-1024..1024).random()
+        assert(fastPower(x, 0) == 1)
+    }
+
+    @ParameterizedTest
+    @MethodSource("fastPowerWithIndex")
+    fun `anything to the power of 1 is itself`(fastPowerWithIndex: FastPowerWithIndex) {
+        val fastPower = fastPowerWithIndex.fastPower
+
+        val x = (-1024..1024).random()
+        assert(fastPower(x, 1) == x)
+    }
+
+    @ParameterizedTest
+    @MethodSource("fastPowerWithIndex")
+    fun `fast power works`(fastPowerWithIndex: FastPowerWithIndex) {
+        val fastPower = fastPowerWithIndex.fastPower
+
+        val x = (0..42).random()
+        val n = (0..5).random()
+        assert(fastPower(x, n) == x.toDouble().pow(n).roundToInt()) {
+            "${fastPower(x, n)} is not ${x.toDouble().pow(n).roundToInt()}"
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("fastPowerWithIndex")
+    fun `fast power (large power)`(fastPowerWithIndex: FastPowerWithIndex) {
+        val fastPower = fastPowerWithIndex.fastPower
+
         val x = (2..3).random()
         val n = (2..15).random()
-        functions.forEach{ fastPower ->
-            assert(fastPower(x, n) == x.toDouble().pow(n).roundToInt()) {
-                "${fastPower(x, n)} is not ${x.toDouble().pow(n).roundToInt()}"
-            }
+        assert(fastPower(x, n) == x.toDouble().pow(n).roundToInt()) {
+            "${fastPower(x, n)} is not ${x.toDouble().pow(n).roundToInt()}"
         }
     }
 
-    @Test
-    fun `fast power for powers of two`() {
-        functions.forEach { fastPower -> assert(fastPower(2, 6) == 64) }
-        functions.forEach { fastPower -> assert(fastPower(2, 7) == 128) }
-        functions.forEach { fastPower -> assert(fastPower(2, 8) == 256) }
-        functions.forEach { fastPower -> assert(fastPower(2, 9) == 512) }
-        functions.forEach { fastPower -> assert(fastPower(2, 10) == 1024) }
+    @ParameterizedTest
+    @MethodSource("fastPower")
+    fun `fast power for powers of two`(fastPower: (x: Int, n: Int) -> Int) {
+        assert(fastPower(2, 6) == 64)
+        assert(fastPower(2, 7) == 128)
+        assert(fastPower(2, 8) == 256)
+        assert(fastPower(2, 9) == 512)
+        assert(fastPower(2, 10) == 1024)
     }
 }
